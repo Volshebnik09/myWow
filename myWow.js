21446bac6eef1717d5f699bac6d4909ff81fbcf6
Komnatniy_Koldun <82974466+Volshebnik09@users.noreply.github.com>
2022-03-13 03:25:43 +0300

    Add files via upload

--- myWow.js
+++ myWow.js
@@ -4,7 +4,7 @@
  *  где '#block1' - написан по селектору querySelector
  *  устанавливаем анимацию и её скорость
  *  block1.riseLeft('1s'); или block1.riseRight('1s');
- *  скорость ставиться по примеру 0.1s; 1s; 2s и т.п.
+ *  скорость ставим как в css для параметра transition, вполне можно писать так '2s ease-in'
  */
 export default class MyWowComponent{
 	Block;
@@ -20,12 +20,16 @@ export default class MyWowComponent{
 	}
 	rise(animDuration,turn){
 		let transform;
+		let opacity;
 		transform = this.Block.style.transform;
+		opacity = this.Block.style.opacity;
+		this.Block.style.opacity = '0';
 		this.Block.style.transform = `${turn}`;
 		window.addEventListener('scroll',() => {
 			// console.log(this.Block.getBoundingClientRect().y + this.Block.getBoundingClientRect().height - window.innerHeight)
 			if (this.Block.getBoundingClientRect().y - window.innerHeight + this.Block.getBoundingClientRect().height* 0.8 < 0) {
 				setTimeout(() => {
+					this.Block.style.opacity = opacity;
 					this.Block.style.transition = `${animDuration}`
 					this.Block.style.transform = `${transform}`
 				},2)
